c3d6bafa1a9ad66ade1b4d17c8aeaa5b2b75b71b
Robert Boloc <robertboloc@gmail.com>
2012-09-16 13:56:25 +0200

    added .gitignore

--- argus.rb
+++ argus.rb
@@ -9,7 +9,6 @@ class Argus
   @@option_text_only = '|Ff:facetmediatype:a:a:MAT%252BTEXTUAL::'
   @@option_tarragona = '|Ff:facetcollections:124:124:TARRAGONA-P%2525C3%2525BAblica%252Bde%252BTarragona::'
   @@option_spanish = '|Ff:facetlanguages:spa:spa:Spanish::'
-  
   def initialize
     @agent = Mechanize.new
     @agent.user_agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)'
@@ -18,30 +17,30 @@ class Argus
   def search(query)
     url = @@base_url+query.gsub(' ', '+')+@@option_text_only+@@option_tarragona+@@option_spanish
     page = @agent.get(url)
-    
+
     #get page count
     count = page.parser.xpath("//div[@class='pagination1']/a").count
     count == 0 ? count = 1 : nil #if it only has one page
-		
+
     #search for all the pages
     books = Hash.new
     page = 0
     while page < count
-        newbooks = self.get_page(query, page)
-        #merge into main array
-        books = books.merge(newbooks){|key, first, second| first.to_i + second.to_i }
-        page = page + 1
+      newbooks = self.get_page(query, page)
+      #merge into main array
+      books = books.merge(newbooks){|key, first, second| first.to_i + second.to_i }
+      page = page + 1
     end
-    
-	#sort the books
-	books = books.sort
+
+    #sort the books
+    books = books.sort
     #display the books
-	books.each do |book|
-		if book[1].to_i != 1
-		  puts "#{book[0]} x#{book[1]}"
-		else 
-		  puts "#{book[0]}" 
-		end
+    books.each do |book|
+      if book[1].to_i != 1
+        puts "#{book[0]} x#{book[1]}"
+      else
+        puts "#{book[0]}"
+      end
     end
 
   end
@@ -53,18 +52,18 @@ class Argus
     #fetch the content
     books = Hash.new
     page.links.each do |link|
-        if link.attributes['id'] =~ /recordDisplayLink2Component/ 
-          key = link.text.strip
+      if link.attributes['id'] =~ /recordDisplayLink2Component/
+        key = link.text.strip
 
-          if books.has_key?(key)
-            books[key] = books[key].to_i + 1
-          else
-            books[key] = 1
-          end
-
+        if books.has_key?(key)
+          books[key] = books[key].to_i + 1
+        else
+          books[key] = 1
         end
+
+      end
     end
-    
+
     return books
   end
 
